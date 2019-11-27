36de2d3eb3714f97bc7960d45e02ed6da3c7da5f
Gabe cajigas <gabriel.cajigas1@hotmail.com>
2019-11-26 22:01:01 -0500

    first commit

---
+++ src/components/titles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Weather</h1>
+		<h2 className="title-container__subtitle" >Search for your location</h2>
+	</div>
+)
+
+export default Titles;
